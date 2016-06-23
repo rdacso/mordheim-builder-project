7070b7ea4c8b874b1d267220e6e769c01e1e7e47
Rebecca Dacso <rdacso@clearcareonline.com>
2016-06-22 17:39:32 -0700

    the actual mordheim tutorial program

--- mordheim_builder.py
+++ mordheim_builder.py
@@ -1,61 +1,78 @@
-# start with 1 warband  then go to 2 warband
-# if you go to 2 warband, ask the user which warband they want to play
+# need to access the mordheim_classes file
+import mordheim_classes
 
-from firebase import firebase
-firebase = firebase.FirebaseApplication('https://mordheim-builder.firebaseio.com/', None)
-warband_input = {
-  'sisters of sigmar': {
-    'details_query': "If you would like to know more about characters, type 'Characters'.\nIf you would like to know  more about weapons and equipment, type 'Weapons'.\nIf you would like to know more about Prayers of Sigmar, please type 'Magic'. If you would like to move on to character creation, please type 'Move on.'",
-    'options':{
-        'characters': {
-            'details_query': "Who would you like to know more about? To learn more about heros, please enter 'sigmar matriarch', 'sister superior', or 'augur'. If you would like to know about available henchmen, please enter 'sigmarite sister' or 'novice'. If you would like to move on, please enter: 'move on': ",
-            'options': {
-                'sigmar matriarch': {
-                    'description': 'The Sigmarite Matriarchs, of whom there is an inner circle of twelve, are answerable to the High Matriarch of the temple. Each must lead a warband of Sisters in frequent searches of the city in order to purge the ruins. Matriarchs are driven by a zealous devotion to the Cult of Sigmar and a relentless determination to redeem the Sisterhood in His eyes.\nWeapons/Armour: The Sigmarite Matriarch may be equipped with weapons and armour chosen from the Sisters of Sigmar Equipment list.\nSPECIAL RULES Leader: Any warband member within 6" of the Sigmarite Matriarch may use her Leadership characteristic when taking any Leadership tests.\n They have experience 20 and cost 75 gold\n You may only have one matriarch.\n To learn more about the weapons available, please type: weapons\nPrayers of Sigmar: The Matriarch has studied the Prayers of Sigmar. See the Magic section.\n To learn more about the weapons available, please type: weapons. Otherwise type: n ',
-                    'limit': 1
-                },
-                'sister superior': {
-                    'description': 'Each of the Sisters Superior is a long-serving priestess of the Cult of Sigmar, well versed in the rituals of the temple and an example to the younger Sisters and Novices. The Sisters Superior are entrusted with maintaining the faith and fervour of the order. Any peril or foe that may lurk in the ruins of Mordheim is as nothing compared to the wrath of a Sister Superior.\n Weapons/Armour: A Sister Superior may be equipped with weapons and armour chosen from the Sisters of Sigmar Equipment list.\n They have experience 8 and cost 35 gold. \n You are allowed to have up to 3 sister superiors.\n To learn more about the weapons available, please type: weapons. Otherwise type n',
-                    'limit': 3
-                },
-                'augur': {
-                    'description': 'The blind Augurs of the Sisterhood are blessed beyond their comrades. By giving up their sight they have gained something far more, second sight - a gift from their patron god. Only a very few are marked this way, and they are greatly revered by the Sisterhood. Unlike the rest of the priestesses, they shave their heads, save for a single long braid.\nWeapons/Armour: The Augurs may be equipped with weapons chosen from the Sisters of Sigmar Equipment list. They never wear armour.\nlSPECIAL RULES Blessed Sight: An Augur can re-roll any failed characteristic tests (climbing, resisting spells or any other reason), and any rolls to hit in close combat or shooting. You must accept the second result. In addition, an Augur can use her Blessed Sight to help the Sisterhood when they are searching the city for wyrdstone. If the Augur is not put out of action in the battle, you may roll two dice for her in the exploration phase and pick either dice as the result.\nThey have experience 0 and cost 25 gold.\nYou are allowed to have only one Augur.\n To learn more about weapons available, please type: weapons. Otherwise type n',
-                    'limit': 1
-                },
-                'sigmarite sister': {
-                    'description': 'Sigmarite Sisters know that their entire order is shamed in the eyes of their Lord Sigmar. Every one of them is sworn upon His altar to pacify the city and thereby redeem themselves. Whatever the perils and horrors that stand in their way, they will be overcome!\nWeapons/Armour: The Sigmarite Sisters may be equipped with weapons and armour chosen from the Sisters of Sigmar Equipment list.\nThey have experience 0 and cost 25 gold.',
-                },
-                'novice': {
-                    'description': 'By tradition, the Sisters draw their recruits only from the most noble houses of the Empire, and families consider it a great honour to have their daughter accepted into the order. Only maidens of noble lineage can be relied upon to have the devotion to duty and innate sense of honour. Few though the recruits may be, they must endure several years as Novices during which time their devotion will be tested to the full. All are eager to prove themselves worthy to be the handmaidens of Sigmar.\nWeapons/Armour: The Novices may be equipped with weapons and armour chosen from the Sisters of Sigmar Equipment list.\n They have experience 0 and cost 15 gold.',
-                }
-            }
-        }
-    }
-  }
-}
+#user input for warband type. Necessary when there are more warbands to choose from.
+warband_name = raw_input('Which warband do you wish to play as?').strip()
+
+# start of the tutorial. Gives user option to seek help or move on. 
+
+help = raw_input("If you  need assistance, please type 'help'. To move on to warband creation, please type 'move on'").strip()
+
+#This is a warband tutorial. It allows the user to ask questions regarding specific things relating to Sisters of Sigmar
+
+def tutorial():
+    if help == 'help':
+        while True:
+            if warband_name == 'sisters of sigmar'.lower() and help == 'help'.lower():
+                answer = raw_input("\nWhat do you want to know about? Please enter heroes, henchmen, weapons, armour, items, or spells. To exit this tutorial, please type 'move on'").strip()
+            if answer == 'heroes'.lower():
+                print '\nThe leader is the Sigmarite Matriarch. %s' % mordheim_classes.sigmarite_matriarch.description
+                print "\n----------------------"
+                print '\nYou also have the Sister Superiors. %s' % mordheim_classes.sister_superior.description,
+                print '\n______________________'
+                print '\nLastly, you have your Augur %s' % mordheim_classes.augur.description,
+            elif answer == 'henchmen'.lower():
+                print '\nSigmarite Sisters: %s' % mordheim_classes.sigmarite_sister.description,
+                print "\n----------------------"
+                print '\nNovices: %s' % mordheim_classes.novice.description,
+                print "\n----------------------"
+            elif answer == 'weapons':
+                print '\nThe dagger: %s' % mordheim_classes.dagger.description
+                print "\n----------------------"
+                print '\nThe mace: %s' % mordheim_classes.mace.description
+                print "\n----------------------"
+                print '\nThe hammer: %s' % mordheim_classes.hammer.description
+                print "\n----------------------"
+                print '\nThe flail: %s' % mordheim_classes.flail.description
+                print "\n----------------------"
+                print '\nThe double handed weapon: %s' % mordheim_classes.double_handed_weapon.description
+                print "\n----------------------"
+                print '\nThe sling: %s' % mordheim_classes.sling.description
+                print "\n----------------------"
+                print '\nThe sisters have two special weapons: the Sigmarite Warhammer and the Steel Whip.\nThe Sigmarite Warhammer is %s.\nThe Steel Whip is %s' % (mordheim_classes.sigmarite_warhammer.description, mordheim_classes.steel_whip.description)
+            elif answer == 'armour':
+                print '\nLight armour: %s'% mordheim_classes.light_armour.description
+                print "\n----------------------"
+                print '\nHeavy armour: %s' % mordheim_classes.heavy_armour.description
+                print "\n----------------------"
+                print '\nShield: %s' % mordheim_classes.shield.description
+                print "\n----------------------"
+                print '\nBuckler: %s' % mordheim_classes.buckler.description
+                print "\n----------------------"
+                print '\nHelmet: %s' % mordheim_classes.helmet.description
+            elif answer == 'items':
+                print '\nHoly Tome: %s' % mordheim_classes.holy_tome.description
+                print "\n----------------------"
+                print '\nBlessed water: %s' % mordheim_classes.blessed_water.description
+                print "\n----------------------"
+                print '\nHoly relic: %s' % mordheim_classes.holy_relic.description
+            elif answer == 'spells':
+                print '\nHammer of Sigmar: %s' % mordheim_classes.hammer_of_sigmar.description
+                print "\n----------------------"
+                print '\nHeart of Steel: %s' % mordheim_classes.hearts_of_steel.description
+                print "\n----------------------"
+                print '\nSoulFire: %s' % mordheim_classes.soulfire.description
+                print "\n----------------------"
+                print '\nShield of Faith: %s' % mordheim_classes.shield_of_faith.description
+                print "\n----------------------"
+                print '\nHealing Hand: %s' % mordheim_classes.healing_hand.description
+                print "\n----------------------"
+                print '\nArmour of Righteousness: %s' % mordheim_classes.armour_of_righteousness.description
+            else:
+                break
+
+tutorial()
 
 
-
-warband_creation_data = {
-    'characters': {}
-}
-
-def band_choice():
-    warband = raw_input('Which warband would you like to play? ')
-    if warband in warband_input.keys():
-        details = raw_input(warband_input[warband_creation_data[warband].lower()])
-        warband_data = warband_input[warband_creation_data[warband]]
- 
-band_choice()
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
